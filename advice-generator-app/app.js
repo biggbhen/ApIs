2e4895f2d6df8eef7e661ad3d982f8e23e2b7376
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-06-07 12:35:23 +0100

    done for now

--- advice-generator-app/app.js
+++ advice-generator-app/app.js
@@ -1,4 +1,15 @@
 const logData = document.querySelector('.button');
+const word = document.querySelector('.advice');
+const adviceId = document.querySelector('.adviceId');
+const adv = new Advice();
+// const store = new Store();
+
+// const advData = store.getAdviceData();
+// document.addEventListener('DOMContentLoaded', adv.getAdvice);
 logData.addEventListener('click', () => {
-  console.log(' is everything an object?');
+  adv.getAdvice().then((data) => {
+    // console.log(data);
+    word.textContent = data.response.slip.advice;
+    adviceId.textContent = `#${data.response.slip.id}`;
+  });
 });
