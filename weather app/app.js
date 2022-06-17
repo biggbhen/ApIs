675620ea9586f413fa79c40804f9f79bfaf97ad3
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-06-17 15:34:34 +0100

    working on displaying the data on thew UI

--- weather app/app.js
+++ weather app/app.js
@@ -5,12 +5,9 @@ search.addEventListener('click', (e) => {
   const weather = new Weather(
     e.target.parentElement.previousElementSibling.value
   );
-  // console.log(e.target.parentElement.previousElementSibling.value);
-  // console.log(cityVal);
-  weather
-    .getWeather()
-    .then((results) => {
-      console.log(results);
-    })
-    .catch((err) => console.log(err));
+
+  weather.getWeather().then((results) => {
+    console.log(results);
+  });
+  // .catch((err) => console.log(err));
 });
