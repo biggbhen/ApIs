074c27638e162bf4536911eeafdd575c04cf9010
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-07-10 17:00:29 +0100

    working on responsiveness

--- ip_address_tracker/src/App.js
+++ ip_address_tracker/src/App.js
@@ -1,10 +1,12 @@
 import Header from './components/header/Header';
 import './App.css';
+import Map from './components/map/Map';
 
 function App() {
 	return (
 		<div className='App'>
 			<Header />
+			<Map />
 		</div>
 	);
 }
---
+++ ip_address_tracker/src/components/map/Map.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './map.css'
+
+const Map = () => {
+  return (
+    <div className='mapDisplay'></div>
+  )
+}
+
+export default Map
