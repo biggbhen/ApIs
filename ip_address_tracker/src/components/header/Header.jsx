a10fcbb7fea7865775428503423866deddd646f0
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-07-10 16:09:58 +0100

    styling the location details section

--- ip_address_tracker/src/components/header/Header.jsx
+++ ip_address_tracker/src/components/header/Header.jsx
@@ -8,9 +8,16 @@ const Header = () => {
     <div className='header'>
     <h1>IP Address Tracker</h1>
     <form action="submit">
-     <input type="text" />
-     <button>hello</button>
+     <input type="text" placeholder='search for any ip address or domain'/>
+    <button></button>
     </form>
+    <div className='location'>
+     <div></div>
+     <div></div>
+     <div></div>
+     <div></div>
+
+    </div>
     </div>
   )
 }
