f19261f5182fbc7baa190501295801089b294777
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-07-10 14:24:24 +0100

    styling the header

--- ip_address_tracker/src/App.js
+++ ip_address_tracker/src/App.js
@@ -1,9 +1,10 @@
+import Header from './components/header/Header';
 import './App.css';
 
 function App() {
 	return (
 		<div className='App'>
-			<header className='App-header'></header>
+			<Header />
 		</div>
 	);
 }
---
+++ ip_address_tracker/src/components/header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './header.css'
+
+
+
+const Header = () => {
+  return (
+    <div className='header'>
+    <h1>IP Address Tracker</h1>
+    <form action="submit">
+     <input type="text" />
+     <button>hello</button>
+    </form>
+    </div>
+  )
+}
+
+export default Header
