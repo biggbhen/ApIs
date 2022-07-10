266715c5c2ec71db676ccf8cc02f416c84299999
biggbhen <96218403+biggbhen@users.noreply.github.com>
2022-07-10 16:46:46 +0100

    styled the major components

--- ip_address_tracker/src/components/header/Header.jsx
+++ ip_address_tracker/src/components/header/Header.jsx
@@ -11,11 +11,23 @@ const Header = () => {
      <input type="text" placeholder='search for any ip address or domain'/>
     <button></button>
     </form>
-    <div className='location'>
-     <div></div>
-     <div></div>
-     <div></div>
-     <div></div>
+    <div className='locationInfo'>
+     <div className='ipDetails'>
+      <h2>IP ADDRESS</h2>
+      <h3>192.212.174.101</h3>
+      </div>
+     <div className='locationDetails'>
+      <h2>LOCATION</h2>
+      <h3>Brooklyn, NY <br /> 10001</h3>
+      </div>
+     <div className='timezoneDetails'>
+      <h2>TIMEZONE</h2>
+      <h3>UTC-05:00</h3>
+      </div>
+     <div className='ispDetails'>
+      <h2>ISP</h2>
+      <h3>SpaceX <br /> Starlink</h3>
+      </div>
 
     </div>
     </div>
